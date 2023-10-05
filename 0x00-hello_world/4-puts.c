b2147317656baf68f5914943a7d0849be0289836
deonokongo <lilian.deon29@gmail.com>
2023-10-05 11:26:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,8 @@
 int main(void)
 
 {
- puts("Programming is like building a multilingual puzzle");
-  return (0);
+   
+       	puts("\"Programming is like building a multilingual puzzle");
+   	return (0);
+
 }  
