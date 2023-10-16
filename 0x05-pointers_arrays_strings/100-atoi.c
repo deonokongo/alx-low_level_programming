42caaeff0fa7b029e5aff1f288bd6d8c88db5475
deonokongo <lilian.deon29@gmail.com>
2023-10-16 08:29:30 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,24 @@ int _atoi(char *s)
     int sign = 1;
     int result = 0;
 
-    if (s[0] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-
     while (s[i] != '\0')
     {
-        if (s[i] >= '0' && s[i] <= '9')
+        if (s[i] == '-')
+        {
+            sign = -sign;
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
         {
             result = result * 10 + (s[i] - '0');
         }
-        else
+        else if (result > 0)
         {
             break;
         }
+
         i++;
     }
 
-    return sign * result;
+    return (sign * result);
 }
 
