6984187cb037149cd3427e0e5079f629e5d49e6f
deonokongo <lilian.deon29@gmail.com>
2023-11-06 04:38:52 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,12 +63,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
-		
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
-		
 	if (dog->name == NULL)
 	{
 		free(dog);
